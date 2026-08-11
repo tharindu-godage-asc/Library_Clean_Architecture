1192654cffef981121abbe017739ddb2a0ff37c4
TharinduGodage <TharinduGodage@ASC-LT-ENG-THAR>
2026-08-11 10:02:23 +0530

    implement book and member service business logic

--- Library.Application/Services/BookService.cs
+++ Library.Application/Services/BookService.cs
@@ -1,10 +1,51 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using Library.Application.Interfaces;
+using Library.Domain.Entities;
+using Library.Domain.Exceptions;
 
 namespace Library.Application.Services
 {
-    internal class BookService
+    public class BookService
     {
+        private readonly IBookRepository _bookRepository;
+        private readonly IUnitOfWork _unitOfWork;
+
+        public BookService(
+            IBookRepository bookRepository,
+            IUnitOfWork unitOfWork)
+        {
+            _bookRepository = bookRepository;
+            _unitOfWork = unitOfWork;
+        }
+
+        public async Task<IEnumerable<Book>> GetAllAsync()
+        {
+            return await _bookRepository.GetAllAsync();
+        }
+
+        public async Task<Book> GetByIdAsync(int id)
+        {
+            return await _bookRepository.GetByIdAsync(id)
+                   ?? throw new NotFoundException("Book not found.");
+        }
+
+        public async Task CreateAsync(Book book)
+        {
+            var existingBook = await _bookRepository.GetByIsbnAsync(book.Isbn);
+
+            if (existingBook is not null)
+                throw new ConflictException("A book with this ISBN already exists.");
+
+            await _bookRepository.AddAsync(book);
+            await _unitOfWork.SaveChangesAsync();
+        }
+
+        public async Task DeleteAsync(int id)
+        {
+            var book = await _bookRepository.GetByIdAsync(id)
+                       ?? throw new NotFoundException("Book not found.");
+
+            _bookRepository.Delete(book);
+            await _unitOfWork.SaveChangesAsync();
+        }
     }
 }
--- Library.Application/Services/MemberService.cs
+++ Library.Application/Services/MemberService.cs
@@ -1,10 +1,53 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using Library.Application.Interfaces;
+using Library.Domain.Entities;
+using Library.Domain.Exceptions;
 
 namespace Library.Application.Services
 {
-    internal class MemberService
+    public class MemberService
     {
+        private readonly IMemberRepository _memberRepository;
+        private readonly IUnitOfWork _unitOfWork;
+
+        public MemberService(
+            IMemberRepository memberRepository,
+            IUnitOfWork unitOfWork)
+        {
+            _memberRepository = memberRepository;
+            _unitOfWork = unitOfWork;
+        }
+
+        public async Task<IEnumerable<Member>> GetAllAsync()
+        {
+            return await _memberRepository.GetAllAsync();
+        }
+
+        public async Task<Member> GetByIdAsync(int id)
+        {
+            return await _memberRepository.GetByIdAsync(id)
+                   ?? throw new NotFoundException("Member not found.");
+        }
+
+        public async Task CreateAsync(Member member)
+        {
+            var existingMember =
+                await _memberRepository.GetByEmailAsync(member.Email);
+
+            if (existingMember is not null)
+                throw new ConflictException(
+                    "A member with this email already exists.");
+
+            await _memberRepository.AddAsync(member);
+            await _unitOfWork.SaveChangesAsync();
+        }
+
+        public async Task DeleteAsync(int id)
+        {
+            var member = await _memberRepository.GetByIdAsync(id)
+                         ?? throw new NotFoundException("Member not found.");
+
+            _memberRepository.Delete(member);
+            await _unitOfWork.SaveChangesAsync();
+        }
     }
 }
