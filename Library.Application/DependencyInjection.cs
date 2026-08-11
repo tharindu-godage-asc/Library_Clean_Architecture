4bf5d8f57168a127f7c5a1041feb9ae2e4787c9b
TharinduGodage <TharinduGodage@ASC-LT-ENG-THAR>
2026-08-11 11:11:06 +0530

    implement clean architecture core layers

--- Library.Application/DependencyInjection.cs
+++ Library.Application/DependencyInjection.cs
@@ -1,10 +1,18 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using Library.Application.Services;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace Library.Application
 {
-    internal class DependencyInjection
+    public static class DependencyInjection
     {
+        public static IServiceCollection AddApplication(
+            this IServiceCollection services)
+        {
+            services.AddScoped<BookService>();
+            services.AddScoped<MemberService>();
+            services.AddScoped<BorrowingService>();
+
+            return services;
+        }
     }
 }
